dccc10b001b1c8ed36352ea4bac665b691d88cd2
Lucas Yudi N. Nishimoto <70718279+lucasnishimoto@users.noreply.github.com>
2023-02-13 09:45:25 -0300

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 echo "Verificando se tem o docker..."
 docker version 
-if [ $? -eq 0 ]
-	then 
-		echo "Voce já possui o docker"
-		exit
+if [ $? -eq 0 ]; then 
+	echo "Voce já possui o docker"
+	exit
 else
-		echo "Você não possui o docker, deseja instalar ? [ S / N]"
-		read RESPONSE
-			if [[ $RESPONSE != 'N' && $RESPONSE != 'n' ]]; then
-				then
-					echo "Instalando o docker..."
-					curl -sSL get.docker.com | bash 
-			else 
-					echo "Ok, bye !"
-					exit
-		fi 
+	echo "Você não possui o docker, deseja instalar ? [ S / N]"
+	read RESPONSE
+		if [[ $RESPONSE != 'N' && $RESPONSE != 'n' ]]; then
+			echo "Instalando o docker..."
+			curl -sSL get.docker.com | bash 
+		else 
+			echo "Ok, bye !"
+			exit
+	fi 
 fi
 
 ########################################
